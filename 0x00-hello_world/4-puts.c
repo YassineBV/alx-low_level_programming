ea72c814a57c5436c211449b0216176b71fb0535
YassineBV <businessarto@gmail.com>
2023-08-10 15:31:01 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
 /**
+ * This is a multiline comment
  * main is the entry point of my program
  * Return: Always 0 (success)
  */
@@ -11,6 +12,6 @@ int main(void)
 	char first[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(first);
-	
+
 	return (0);
 }
