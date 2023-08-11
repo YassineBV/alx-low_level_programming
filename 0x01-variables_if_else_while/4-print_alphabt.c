3fed0f197785d27969f16190d9fa95c4100cb00a
YassineBV <businessarto@gmail.com>
2023-08-11 13:12:53 -0700

    4-task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+# include <stdlib.h>
+/**
+ * main - entry of the program
+ *
+ *Description: program that prints the alphabet in lowercase
+ *
+ *Return: Always 0 (success)
+*/
+int main(void)
+{
+	char noeq;
+	
+	for (noeq = 'a'; noeq <= 'z'; noeq++)
+	{
+		if (noeq != 'e' && noeq != 'q')
+		putchar(noeq);
+	}
+	
+	putchar('\n');
+	return (0);
+}
