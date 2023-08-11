a773050547ca24d57612ce239d8acfc08a68fc74
YassineBV <businessarto@gmail.com>
2023-08-11 03:57:40 -0700

    0-task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,11 +19,11 @@ int main(void)
 	}
 	else if (n < 0)
 	{
-		printf("%d is positive\n");
+		printf("%d is positive\n", n);
 	}
 	else
 	{
-		printf("%d is zero\n");
+		printf("%d is zero\n", n);
 	}
 
 	return (0);
