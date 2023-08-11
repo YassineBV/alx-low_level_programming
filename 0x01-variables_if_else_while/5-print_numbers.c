f6772e88ea52b2502813620dfac75041435a89b3
YassineBV <businessarto@gmail.com>
2023-08-11 13:40:35 -0700

    5-task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry of the program
+ *
+ *Description: program that prints all single digit numbers
+ *
+ *Return: Always 0 (success)
+*/
+int main(void)
+{
+	int raem = '0';
+
+	for (;raem <= '9'; raem++)
+	printf("%c", raem);
+
+	return (0);
+}
