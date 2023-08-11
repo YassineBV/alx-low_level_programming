eef9a99d457ac9a4ff211e17a76fe9b09866fe28
YassineBV <businessarto@gmail.com>
2023-08-11 08:54:46 -0700

    2-task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+# include <stdlib.h>
+/**
+ * main - number stored in the variable n is positive or negative
+ *
+ *Description: his program assign a random number to the variable n
+ *
+ *Return: Always 0 (success)
+*/
+int main(void)
+{
+	char var;
+	for (var = 'a'; var <= 'z'; var++)
+	{
+	putchar(var);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
+
+
