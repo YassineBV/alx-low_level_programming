cd7d8675067255ae99059f5953ba4055eff9804a
YassineBV <businessarto@gmail.com>
2023-08-12 04:12:55 -0700

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	char bpl = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char bpl[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, bpl, 59);
 
