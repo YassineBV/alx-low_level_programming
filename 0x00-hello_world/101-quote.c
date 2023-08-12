21a367fbcdc86f23b3fac77c20728368eef5850e
YassineBV <businessarto@gmail.com>
2023-08-12 03:36:47 -0700

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - entry of the program
+ *
+ *Description: program prints message to the standard error
+ *
+ *Return: Always 0 (success)
+*/
+int main(void)
+{
+	const char *toerror = "and that piece of art is useful\" - Dora Korpar, 2015-10-19.\n";
+	ssize_t n = write(2, toerror, strlen(toerror));
+
+	return (0);
+}
