a87cc038c72684b629aafce64f2bc3e1bf8a15ef
YassineBV <businessarto@gmail.com>
2023-08-12 00:39:19 -0700

    5-task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry of the program
+ *
+ *Description: prints with proper grammar
+ *
+ *Return: Always 0 (success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	
+	return (0);
+}
