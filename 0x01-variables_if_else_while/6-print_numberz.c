735da1946b5e76ddda8290b2e67334d5dab58e21
YassineBV <businessarto@gmail.com>
2023-08-12 23:47:06 -0700

    6-task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry of the program
+ *
+ *Description: prints all single digit numbers of base 10 starting from 0
+ *
+ *Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int kol = '0';
+
+	while ( kol <= 9)
+	{
+	putchar(kol);
+	kol++;
+	}
+	putchar('\n');
+	return (0);
+}
+
+
+
