709be69f49feea221090bb193c2faf645ca33bc4
YassineBV <businessarto@gmail.com>
2023-08-13 05:28:23 -0700

    ptint-combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - entry of the program
+ *
+ *Description: program prints all possible different combinations
+ *
+ * Description: of two digits
+ *
+ *Return: Always 0 (success)
+*/
+int main(void)
+{
+	int ra1, ra2;
+	for (ra1 = '0'; ra1 <= '9'; ra1++)
+	{
+		for (ra2 = '0'; ra2 <= '9'; ra2++)
+		{
+			if (ra1 != ra2 && ra1 < ra2)
+			{
+				putchar(ra1);
+				putchar(ra2);
+				if (ra1 != '8' || ra2 != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
