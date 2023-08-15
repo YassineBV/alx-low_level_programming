78bde119770d88c03f92ceffa23cebe9ad57d9dc
YassineBV <businessarto@gmail.com>
2023-08-15 03:11:52 -0700

    0-tsk

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+/**
+ * main - entry of the program
+ *
+ *Description: program that prints a string
+ *
+ *Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char  char_string[] = '_putchar';
+	int wr;
+
+	for (wr = 0; wr < 8; wr++)
+		_putchar(char_string[wr]);
+
+	-putchar('\n');
+	return (0);
+}
