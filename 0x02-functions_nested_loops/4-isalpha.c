05feed5c4e40d666a99af6521274f64c259b1b16
YassineBV <businessarto@gmail.com>
2023-08-15 07:18:30 -0700

    4-task

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 /**
  * _isalpha - entry of the program
  *
- *Description: checks for alphabetic character
+ *@c: checks for alphabetic character
  *
  *Return: Always 0 (success)
 */
@@ -12,7 +12,7 @@ int _isalpha(int c)
 
 	if (c >= 65 && c <= 90)
 		return (1);
-	else if ( c >= 97 && c <= 122)
+	else if (c >= 97 && c <= 122)
 		return (1);
 	else
 		return (0);
