63ed2349b316e8f25e5fd25b503d3bbdcda9b2ad
YassineBV <businessarto@gmail.com>
2023-08-15 04:40:06 -0700

    2-task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+nclude "main.h"
+/**
+ * main - entry of the program
+ *
+ *Description: program that  prints 10 times the alphabet
+ *
+ *Return: Always 0 (success)
+*/
+void print_alphabet_x10(void)
+{
+	char str[] = "abcdefghijklmnopqrstuvwxyz";
+	int rep;
+	int abc;
+	for (rep = 0; rep < 10; i++)
+	{
+		for (abc = 0; abc < 26; abc++)
+			_putchar(str[abc]);
+	}
+	_putchar('\n');
+}
