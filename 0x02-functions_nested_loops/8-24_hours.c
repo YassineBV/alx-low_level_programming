a0cbd991fb2d9f84374748df78fedb13981caaf1
YassineBV <businessarto@gmail.com>
2023-08-15 14:57:39 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 /**
  * jack_bauer - entry of the program
  *
- *Description: program that prints every minute of the day 
+ *Description: program that prints every minute of the day
  *
  *Return: Always 0 (success)
 */
@@ -12,14 +12,15 @@ void jack_bauer(void)
 
 	for (td1 = 0; td1 < 24; td1++)
 	{
-	       	for (td2 = 0; td2 < 60 ; td2++)
-		 {
-			 _putchar(td1 / 10 + 48);
-			 _putchar(td1 % 10 + 48);
-			 _putchar(':');
-			 _putchar(td2 / 10 + 48);
-			 _putchar(td2 % 10 + 48);
-		 }
+		for (td2 = 0; td2 < 60 ; td2++)
+		{
+			_putchar(td1 / 10 + 48);
+			_putchar(td1 % 10 + 48);
+			_putchar(':');
+			_putchar(td2 / 10 + 48);
+			_putchar(td2 % 10 + 48);
+			_putchar('\n');
+		}
 	}
 
 }
