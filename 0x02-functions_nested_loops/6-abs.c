f9e1c99a1c9933ae6749f0644d89bcac5b9c161b
YassineBV <businessarto@gmail.com>
2023-08-15 08:04:06 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _abs - entry of the program
+ *
+ *@int: program that computes the absolute value of an integer
+ *
+ *Return: Always 0 (success)
+*/
+int _abs(int)
+{
+	int np;
+
+	if (int > 0)
+	{
+		np = int - 0
+		_putchar(np);
+		_putchar('\n')
+	}
+	if else (int < 0)
+	{
+		np = int + int * 2
+		_putchar(np);
+		_putchar('\n');
+	}
+	else 
+		_putchar(0);
+	-putchar('\n');
+}
+
