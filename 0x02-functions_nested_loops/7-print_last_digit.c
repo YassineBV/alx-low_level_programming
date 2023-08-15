b4caf4d8c33cc254598480560dcf46048007d95f
YassineBV <businessarto@gmail.com>
2023-08-15 13:32:57 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - entry of the program
+ *
+ *@ld: program  prints the last digit of a number
+ *
+ *Return: Always 0 (success)
+*/
+int print_last_digit(int ld)
+{
+
+	return (ld % 10);
+}
