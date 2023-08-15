ddc888c842833b4df45ddab325bd4f777e2a8c9a
YassineBV <businessarto@gmail.com>
2023-08-15 14:04:14 -0700

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 /**
  * print_last_digit - entry of the program
  *
- *@ld: program  prints the last digit of a number
+ *@l: program  prints the last digit of a number
  *
  *Return: Always 0 (success)
 */
@@ -12,7 +12,9 @@ int print_last_digit(int l)
 	int ld = l % 10;
 
 	if (ld < 0)
-	       ld = ld * -1;	
+	{
+		ld = ld * -1;
+	}
 	_putchar(ld + 48);
 	return (ld);
 }
