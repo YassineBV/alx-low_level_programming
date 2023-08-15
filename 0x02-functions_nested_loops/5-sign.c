91db2c6205da5679c36f2e00c133469b29d7be2e
YassineBV <businessarto@gmail.com>
2023-08-15 07:33:43 -0700

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - entry of the program
+ *
+ *@n: program that prints the sign of a number
+ *
+ *Return: Always 0 (success)
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar(43);
+		_putchar(',');
+		return (1);
+	else if (n == 0)
+		_putchar(48);
+	_putchar(',');
+	return (0);
+	else
+		_putchar(45);
+	_putchar(',');
+	return (-1)
+}
+
