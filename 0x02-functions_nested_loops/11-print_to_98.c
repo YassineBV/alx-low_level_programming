c78d364c68625e8d3b52755dad4b52873b73dfc3
YassineBV <businessarto@gmail.com>
2023-08-16 13:02:25 -0700

    11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_to_98 - entry of the program
+ *
+ *@n: prints all natural numbers from n to 98
+ *
+*/
+void print_to_98(int n)
+{
+	int n = 98;
+
+	if (n < 98)
+	{
+		n++;
+	}
+
+	else
+	{
+		n--;
+	}
+       	_putchar(',');
+	_putchar(' ');
+}	
