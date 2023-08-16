9c142f12c594b3db2f7b69315fedcffcf17a7a01
YassineBV <businessarto@gmail.com>
2023-08-16 10:50:21 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * add - entry of the program
+ *
+ *Description: program  adds two integers
+ *
+ *Return: Always 0 (success)
+*/
+int add(int, int);
+{
+	int a, b;
+	int add;
+	add = a +b;
+
+		if (add <= 9)
+		{
+			_putchar(add + 48);
+		}
+		else(add > 9)
+		{
+			_putchar(add / 10 + 48);
+			_putchar(add % 10 + 48);
+		}
+	return ('\n');
+}
