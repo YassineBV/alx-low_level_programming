9e553fffc0111bed6c92e6dbe76bf6b5ded03ddc
YassineBV <businessarto@gmail.com>
2023-08-21 04:04:58 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - entry of the program
+ *
+ *Description: program that prints the alphabet in lower_uppercase
+ *
+ *Return: Always 0 (success)
+*/
+void print_rev(char *s)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s + 0);
+		s--;
+	}
+	_putchar('\n');
+}
+
