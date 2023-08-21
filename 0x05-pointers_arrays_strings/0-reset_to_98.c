b975177cf6fd0be1d1b35886e4ef9484d3af12f9
YassineBV <businessarto@gmail.com>
2023-08-21 00:59:04 -0700

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,16 +2,18 @@
 /**
  * reset_to_98 - entry of the program
  *
- *@*n: updates the value it points to to 98
+ *@n: updates the value it points to to 98
  *
  *Return: void
 */
 void reset_to_98(int *n)
 {
 	int o = 402;
+
 	n = &o;
 	*n = 98;
 
 _putchar(o / 10 + 48);
 _putchar(o % 10 + 48);
+_putchar('\n');
 }
