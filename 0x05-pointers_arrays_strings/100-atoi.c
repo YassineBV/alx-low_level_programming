a43382cf6a4547d915e828ba88475097e0bbdcfe
YassineBV <businessarto@gmail.com>
2023-08-21 14:49:08 -0700

    task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,16 +11,14 @@ int _atoi(char *s)
 	int po = 1;
 	int ng = 0;
 
-	do
-	{
+	do{
 		if (*s == '-')
 			po *= -1;
 		else if (*s >= '0' && *s <= '9')
 			ng = (ng * 10) + (*s - '0');
 		else if (ng > 0)
 			break;
-	}
-	while (*s++);
+	} while (*s++);
 
 	return (po * ng);
 }
