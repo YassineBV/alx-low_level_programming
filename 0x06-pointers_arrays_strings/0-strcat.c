e4d0057c57859ee8af265beffdcc42b8ab8856cf
YassineBV <businessarto@gmail.com>
2023-08-23 11:33:22 -0700

    task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strcat -  function that concatenates two strings
+ *_strcat - function that concatenates two strings
  *
  *@src: appends the src string
  *@dest: overwriting the terminating null byte
  *
- *Return: a pointer to the resulting string dest
+ *Return: a pointer to the resulting string
 */
 char *_strcat(char *dest, char *src)
 {
