76ae49b270c159594398c0204bc0ffa942fa873a
YassineBV <businessarto@gmail.com>
2023-08-24 04:08:11 -0700

    task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,12 +10,12 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1 [i] != '\0' && s2[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 		{
 			s1++;
 			s2++;
 		}
-	int comp = _strcmp(s1[i],s2[i]);
+	int comp = _strcmp(s1[i], s2[i]);
 	
 	if (comp == 0)
 
