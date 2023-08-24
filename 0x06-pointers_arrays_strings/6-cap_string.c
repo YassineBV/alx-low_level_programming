cf4bd29ad006de68610a5f981695c37a237b9b65
YassineBV <businessarto@gmail.com>
2023-08-24 12:45:33 -0700

    task

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * main - entry of the program
+ *
+ *Description: program that prints the alphabet in lower_uppercase
+ *
+ *Return: Always 0 (success)
+*/
+char *cap_string(char *cap)
+{
+	bool capwrd = true;
+	int i;
+
+	for (i = 0; cap[i] != '\0'; i++)
+	{
+		if (cap[i] == ',' || cap[i] == ';' || cap[i] == '.' || cap [i] == '!' || cap[i] == '?' || cap[i] == '"' || cap[i] == '(' || cap[i] == ')' || cap[i] == '{' || cap[i] == '}')
+		{
+			capwrd = true;
+		}
+		else if ((cap[i} >= 'a' && cap[i] <= 'z') || (cap[i] >= 'A' && cap[i] <= 'Z'))
+		{
+			if (capwrd)
+			{
+				cap[i] = cap[i] - 'a' + 'A';
+				capwrd = false;
+			}
+		}
+		return (cap);
+}
