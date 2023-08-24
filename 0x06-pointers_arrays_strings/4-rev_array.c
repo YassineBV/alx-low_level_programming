013dfb37e71199e58f4631246cfda339bfc3c77d
YassineBV <businessarto@gmail.com>
2023-08-24 10:57:51 -0700

    task

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - entry of the program
+ *
+ *Description: program that prints the alphabet in lower_uppercase
+ *
+ *Return: Always 0 (success)
+*/
+void reverse_array(int *a, int n)
+{
+	int i, revtm;
+	for (i = 0; i <=  n/2; i++)
+	{
+		revtm = a[i]
+		a[i] = a[n -i - 1];
+		a[n - i - 1] = revtm
+	}
+	_putchar(a[i] + 48);
+	_putchar(a[n - i - 1) + 48);
+}
+
