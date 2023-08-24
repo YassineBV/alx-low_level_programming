ec569ec6008a3951ddf70cef432125af4b757b8b
YassineBV <businessarto@gmail.com>
2023-08-24 10:32:38 -0700

    task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,16 +15,16 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] != '\0' && s2[j] != '\0')
 	{
 		if (s1[i] < s2[j])
-			return -1;
+			return (-1);
 		else if (s1[i] > s2[j])
-			return 1;
+			return (1);
 		i++;
 		j++;
 	}
 	if (s1[i] == '\0' && s2[j] == '\0')
-		return 0;
+		return (0);
 	else if (s1[i] == '\0')
-		return -1;
+		return (-1);
 	else
-		return 1;
+		return (1);
 }
