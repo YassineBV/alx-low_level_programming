6c1079050605653f070073f35bf4cd18c562ce19
YassineBV <businessarto@gmail.com>
2023-08-24 03:00:15 -0700

    task

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,7 +4,8 @@
  *
  *@dest: program that copies a string.
  *@src: function should work exactly like strncpy
-
+ *@n: will use at most n bytes from src
+ *
  *Return: pointer to dest
 */
 char *_strncpy(char *dest, char *src, int n)
@@ -12,12 +13,12 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src [i];
+		dest[i] = src[i];
 
 	while (i < n)
-		{
-			dest[i] = '\0';
-			i++;
-		}
+	{
+		dest[i] = '\0';
+		i++;
+	}
 	return (dest);
 }
