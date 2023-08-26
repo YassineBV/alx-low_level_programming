6b6d839674653a01bb04a4bb6053f6a12e0b130d
YassineBV <businessarto@gmail.com>
2023-08-26 05:59:27 -0700

    task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,28 +13,29 @@ void print_times_table(int n)
 	if (n <= 15 && n >= 0)
 	{
 		for (num = 0; num <= n; num++)
-			_putchar('0');
-		for (mt = 1; mt <= n; mt++)
 		{
-			_putchar(',');
-			_putchar(' ');
-
-			rs = num * mt;
-
-			if (rs <= 9)
-				_putchar(' ');
-			if (rs <= 99)
-				_putchar(' ');
-			if (rs >= 100)
+			_putchar('0');
+			for (mt = 1; mt <= n; mt++)
 			{
-				_putchar((rs / 100) + 48);
-				_putchar((rs / 10) % 10 + 48);
+				_putchar(',');
+				_putchar(' ');
+
+				rs = num * mt;
+
+				if (rs <= 9)
+					_putchar(' ');
+				if (rs <= 99)
+					_putchar(' ');
+				if (rs >= 100)
+				{
+					_putchar((rs / 100) + 48);
+					_putchar((rs / 10) % 10 + 48);
+				}
+				else if (rs <= 99 && rs >= 10)
+					_putchar((rs / 10) +48);
+				_putchar((rs % 10) + 48);
 			}
-			else if (rs <= 99 && rs >= 10)
-				_putchar((rs / 10) +48);
-		       _putchar((rs % 10) + 48);
-		}
 		_putchar('\n');
+		}
 	}
 }
-
