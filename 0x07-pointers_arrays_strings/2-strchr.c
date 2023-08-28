029d1b6e80eb30501447f857c50ecc885330b77e
YassineBV <businessarto@gmail.com>
2023-08-28 08:07:59 -0700

    task

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,7 +8,7 @@
 */
 char *_strchr(char *s, char c)
 {
-	unsigned int i;
+	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
@@ -17,5 +17,6 @@ char *_strchr(char *s, char c)
 			return (s + i);
 		}
 	}
-	return (0);
+
+	return ('\0');
 }
