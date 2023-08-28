2946977797de753a08dc97a3ede99cdc5d705dbc
YassineBV <businessarto@gmail.com>
2023-08-28 10:53:16 -0700

    task

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,3 +8,19 @@
  *
  *Return:  pointer to the memory area s
  */
+char *_strpbrk(char *s, char *accept)
+{
+	int is, ja;
+
+	for (is = 0; s[is] != '\0'; is++)
+	{
+		for (ja = 0; accept[ja] != '\0'; ja++)
+		{
+			if (s[is] == accept[ja])
+				return (s + is);
+			else
+				return (NULL);
+		}
+	}
+	return (s +is);
+}
