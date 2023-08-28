038b513e6333d65ed96e7e3be0c8dcfc3a15453f
YassineBV <businessarto@gmail.com>
2023-08-28 10:07:47 -0700

    task

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _memset - entry of the program
+ *
+ *@s: function that fills memory with a constant byte.
+ *@b: constant byte
+ *@n: n bytes
+ *
+ *Return:  pointer to the memory area s
+ */
