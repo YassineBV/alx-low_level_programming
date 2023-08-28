10b01cffaa6d50ab38740cebaaca2399d24c752d
YassineBV <businessarto@gmail.com>
2023-08-28 07:51:06 -0700

    task

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - entry of the program
+ *
+ *Description: program that prints the alphabet in lower_uppercase
+ *
+ *Return: Always 0 (success)
+*/
+char *_strchr(char *s, char c);
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] = c)
+		{
+			return (s);
+		}
+		else 
+			return (NULL);
+	}
+	return (0);
+}
