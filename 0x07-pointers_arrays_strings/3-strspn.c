b57af80de0c116cbf67cc243b7f828469645e86f
YassineBV <businessarto@gmail.com>
2023-08-28 08:55:43 -0700

    task

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -18,7 +18,7 @@ unsigned int _strspn(char *s, char *accept)
 		{
 			for (j = 0; accept[j] != '\0'; j++)
 			{
-				while (s[i] == accept[j])
+				if (s[i] == accept[j])
 				{
 					cont++;
 				}
