ab7764d17f353f3baf32f1d5099454151f8cb4ce
YassineBV <businessarto@gmail.com>
2023-08-29 06:23:08 -0700

    task

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -17,9 +17,9 @@ void print_diagsums(int *a, int size)
 	{
 		for (j = 0; j < size; j++)
 		{
-			if (i == j || i + j = size - 1)
+			if (i == j || i + j == size - 1)
 			{
-				sum += a[i][j];
+				sum += a[i * size + j];
 			}
 		}
 	}
