668bdf4c3a981b3535e7efaf26f322070483537b
YassineBV <businessarto@gmail.com>
2023-08-29 11:52:51 -0700

    task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - entry of the program
+ *
+ *Description:function that prints the numbers, from 0 to 9
+ *
+ *Return: void
+*/
+void print_numbers(void)
+{
+	int nmln;
+
+	for (nmln = 0; nmln < 9; nmln++)
+	{
+		_purchar(nmln + 48);
+	}
+	_putchar('\n');
+}
