386939c0ed16c5ad7c58a981545c18454d3b0443
YassineBV <businessarto@gmail.com>
2023-08-29 11:40:51 -0700

    task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - entry of the program
+ *
+ *Description: program that prints the alphabet in lowercase
+ *
+ *Return: Always 0 (success)
+*/
+int mul(int a, int b)
+{
+	int mpl = a * b;
+
+	return (mpl);
+}
