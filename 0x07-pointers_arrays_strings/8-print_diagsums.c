e72afa03baadfa96e120cb471fe7165b2b12452f
YassineBV <businessarto@gmail.com>
2023-08-29 06:09:21 -0700

    task

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _memset - entry of the program
+ *
+ *@s: function that fills memory with a constant byte.
+ *@b: constant byte
+ *@n: n bytes
+ *
+ *Return:  pointer to the memory area s
+ */
+void print_diagsums(int *a, int size)
+{
+	long long i, j;
+	long long sum = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j || i + j = size - 1)
+			{
+				sum += a[i][j];
+			}
+		}
+	}
+	printf("%lld, ", sum);
+}
