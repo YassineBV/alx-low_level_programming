9f68e10e3d8ccef11b73861c2d99632d19c51ac6
YassineBV <businessarto@gmail.com>
2023-08-30 12:11:53 -0700

    task

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,12 +6,6 @@
  *
  *Return: Always 0 (success)
 */
-int is_prime_number(int n)
-{
-	if (n <= 1)
-		return (0);
-	return (chprm(int n, 2));
-
 int chprm(int n, int dvt)
 {
 	if (dvt * dvt > n)
@@ -20,4 +14,11 @@ int chprm(int n, int dvt)
 		return (0);
 	else 
 		return (chprm(n, dvt + 1));
+}
+
+int is_prime_number(int n)
+{
+        if (n <= 1)
+                return (0);
+        return (chprm(n, 2));
 }
