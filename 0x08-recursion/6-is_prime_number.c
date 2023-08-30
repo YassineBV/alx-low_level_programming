88b00ca60299554a6bf7312be23bec078c31bfb8
YassineBV <businessarto@gmail.com>
2023-08-30 10:29:53 -0700

    task

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - entry of the program
+ *
+ *Description: program that prints the alphabet in lowercase
+ *
+ *Return: Always 0 (success)
+*/
+int is_prime_number(int n)
+{
+	if (n = n * 1)
+		return (1);
+	return (0);
+}
