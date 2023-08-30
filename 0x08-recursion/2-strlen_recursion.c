23010c498f287158bc719e2386209a9cbd1d3a32
YassineBV <businessarto@gmail.com>
2023-08-30 04:13:34 -0700

    task

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strlen_recursion - entry of the program
+ *
+ *@s:  function that returns the length of a string
+ *
+ *Return: returns the length of a string
+*/
+int _strlen_recursion(char *s)
+{
+	int lentk = 0;
+
+	if (s[i] == '\0')
+		return (0);
+	lentk++;
+	lentk += _strlen_recursion(s + 1);
+
+	return (lentk);
+
+	
+
+
