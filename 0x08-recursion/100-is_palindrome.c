2fa77b7888bd7feef8b1d4cbca831d215c5f8351
YassineBV <businessarto@gmail.com>
2023-08-31 04:21:58 -0700

    tsk

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * main - entry of the program
+ *
+ *Description: program that prints the alphabet in lowercase
+ *
+ *Return: Always 0 (success)
+*/
+int letns(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (1 + letns(s + 1));
+}
+
+int is-s-palind(char *s, int begl,int enl)
+{
+	if (begl >= enl)
+		return (1);
+
+	if (s[begl] != s[enl])
+		return (0);
+	return (is-s-palind(s, begl + 1, enl - 1))
+}
+
+int is_palindrome(char *s)
+{
+	int lentofs = letns;
+	return (is-s-palind(s, 0 ,lentofs - 1));
+}			
