095087e268e0b8c58627c64306863ce3cf01c8b9
YassineBV <businessarto@gmail.com>
2023-09-01 11:03:16 -0700

    task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+# include <stdlib.h>
+/**
+ * main - entry of the program
+ *
+ *@argc: program that multiplies two numbers
+ *@argv: print the result, followed by a new line
+ *Return: Always 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+
+	int b1 = atoi(argv[1]);
+	int b2 = atoi(argv[2]);
+
+	int mulresult = b1 * b2;
+
+	  if ( argc != 3)
+        {
+                printf("Error\n");
+                return (1);
+        }
+	
+	printf("%d\n", mulresult);
+
+	return (0);
+}
