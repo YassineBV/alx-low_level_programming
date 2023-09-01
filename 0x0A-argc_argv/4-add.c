4195be4372559a54c6a9bdb79e44f9a37401f2c2
YassineBV <businessarto@gmail.com>
2023-09-01 13:11:02 -0700

    task

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include <ctype.h>
+# include <stdlib.h>
+/**
+ * main - entry of the program
+ *
+ *@argc:  program that adds positive numbers.
+ *@argv:Print the result, followed by a new line
+ *
+ *Return: Always 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+	int sumof, i, j;
+
+	sumof = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sumof += atoi(argv[i]);
+	}
+
+	printf("%d\n", sumof);
+
+	return (0);
+}
