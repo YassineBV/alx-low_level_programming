996af5c9cc115d4587c0b33f25ac5457dfe3d925
YassineBV <businessarto@gmail.com>
2023-09-01 09:36:51 -0700

    task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+# include <stdlib.h>
+/**
+ * main - entry of the program
+ *
+ *Description: program that prints the alphabet in lowercase
+ *
+ *Return: Always 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i <= argc; i++)
+	       printf("%d\n", argv)
+	       return (0);
+}
+
