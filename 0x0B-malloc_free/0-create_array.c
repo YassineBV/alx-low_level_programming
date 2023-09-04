ae72ca7b3b0d8369a3b7d6058d4793e1ed1bf956
YassineBV <businessarto@gmail.com>
2023-09-04 06:16:48 -0700

    task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - function that creates an array of chars
+ *@size: the size of the memory to print
+ *@c: and initializes it with a specific char
+ *
+ *Return: a pointer to the array
+*/
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+
+	char* buffer = (char*)malloc(size * sizeof(char));
+
+	buffer[0] = c;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	if (buffer == NULL)
+	{
+		return (NULL);
+
+	}
+
+	else
+	{
+
+		for (i = 0; i < size; i++)
+		{
+			buffer[i] = c;
+		}
+		return (buffer);
+	}
+	return (buffer);
+}
---
+++ 0x0B-malloc_free/maim.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+char *_memset(char *s, char b, unsigned int n);
+char *create_array(unsigned int size, char c);
+int _putchar(char c);
+
+#endif /* MAIN_H */
