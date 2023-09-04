28d1ef1741c6ede5e527dfc0c12c094e7a99b4a5
YassineBV <businessarto@gmail.com>
2023-09-04 12:27:10 -0700

    task

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * str_concat - function that concatenates two strings 
+ *
+ *@s1: f NULL is passed, treat it as an empty string
+ *@s2:The function should return NULL on failure
+ *Return: pointer to concatstr
+*/
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i,j;
+	char * concatstr;
+
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	j = strlen(s2);
+	i = strlen(s1);
+
+	concatstr = (char *)malloc((i + j) * sizeof(char) + 1);
+
+
+	if (concatstr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+			concatstr[i] = s1[i];
+		}
+		for (j = 0; s2[j] != '\0'; j++)
+		{
+			concatstr[i + j] = s2[j];
+		}
+		concatstr[i + j] = '\0';
+
+		return (concatstr);
+	}
+	return (concatstr);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,5 +6,6 @@ char *_strdup(char *str);
 char *_memset(char *s, char b, unsigned int n);
 char *create_array(unsigned int size, char c);
 int _putchar(char c);
+char *str_concat(char *s1, char *s2);
 
 #endif /* MAIN_H */
