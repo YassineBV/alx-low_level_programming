112f32c7eedd32f38bddfdcf8542d1ec67a384c6
YassineBV <businessarto@gmail.com>
2023-09-05 23:03:28 +0100

    task

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * main - entry of the program
+ *
+ *Description: program that prints the alphabet in lowercase
+ *
+ *Return: Always 0 (success)
+*/
+char *argstostr(int ac, char **av)
+{
+    int i, j;
+    int countav = 0;
+    int strcount = 0;
+    char * strij;
+
+    if (ac == 0 || av == NULL)
+        return (NULL);
+    for (i = 0; i < ac; i++)
+    {
+        strcount++;
+         strcount += strlen(av[i]);
+    }
+    strij = (char *)malloc(strcount + 1);
+    if (strij == 0)
+    {
+        return (NULL);
+    }
+    for (i = 0; i < ac; i++)
+    {
+        for (j = 0; av[i][j] != '\0'; j++, countav++)
+        {
+            strij[countav] = av[i][j];
+        }
+        strij[countav] = '\n';
+        countav++;
+    }
+    strij[countav] = '\n';
+    return (strij);
+}
