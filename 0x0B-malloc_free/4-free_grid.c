6051c51d2b8014b695b990962950dc1572751b52
YassineBV <businessarto@gmail.com>
2023-09-05 15:55:04 +0100

    task

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -9,23 +9,14 @@
 */
 void free_grid(int **grid, int height)
 {
-	int i, j;
-	{
-		for (i = 0; i < height; i++)
+int i;
+    {
+        for (i = 0; i < height; i++)
 		{
-			if (grid[i] == NULL)
-			{
-				for (j = 0; j < i; j++)
-				{
-					free(grid[j]);
-				}
-				free(grid);
-
-			}
-
+			free(grid[i]);
 		}
+		free(grid);
 	}
 }
-	
 
 	
