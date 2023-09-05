6d518999342d350e6a5f9628cdff25b10266495a
YassineBV <businessarto@gmail.com>
2023-09-05 15:59:34 +0100

    task

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,17 +1,17 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * main - entry of the program
+ * free_grid - entry of the program
  *
- *Description: program that prints the alphabet in lowercase
- *
- *Return: Always 0 (success)
+ *@grid: function that frees a 2 dimensional grid
+ *@height: previously created by your alloc_grid function
+ *Return: void
 */
 void free_grid(int **grid, int height)
 {
 int i;
-    {
-        for (i = 0; i < height; i++)
+	{
+		for (i = 0; i < height; i++)
 		{
 			free(grid[i]);
 		}
@@ -19,4 +19,3 @@ int i;
 	}
 }
 
-	
