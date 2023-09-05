7526a486189b57235c50f786299d4178d9994163
YassineBV <businessarto@gmail.com>
2023-09-05 23:11:52 +0100

    task

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,38 +1,38 @@
 #include "main.h"
 /**
- * main - entry of the program
+ * argstostr - entry of the program
  *
- *Description: program that prints the alphabet in lowercase
- *
- *Return: Always 0 (success)
+ *@ac: function that concatenates
+ *@av: all the arguments of your program
+ *Return: a pointer to a new string
 */
 char *argstostr(int ac, char **av)
 {
-    int i, j;
-    int countav = 0;
-    int strcount = 0;
-    char * strij;
+	int i, j;
+	int countav = 0;
+	int strcount = 0;
+	char *strij;
 
-    if (ac == 0 || av == NULL)
-        return (NULL);
-    for (i = 0; i < ac; i++)
-    {
-        strcount++;
-         strcount += strlen(av[i]);
-    }
-    strij = (char *)malloc(strcount + 1);
-    if (strij == 0)
-    {
-        return (NULL);
-    }
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j] != '\0'; j++, countav++)
-        {
-            strij[countav] = av[i][j];
-        }
-        strij[countav] = '\n';
-        countav++;
-    }
-    return (strij);
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		strcount++;
+		 strcount += strlen(av[i]);
+	}
+	strij = (char *)malloc(strcount + 1);
+	if (strij == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, countav++)
+		{
+			strij[countav] = av[i][j];
+		}
+		strij[countav] = '\n';
+		countav++;
+	}
+	return (strij);
 }
