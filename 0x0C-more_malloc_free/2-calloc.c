48d622be1ab28718484871d2dd5eb869c8e11b5c
YassineBV <businessarto@gmail.com>
2023-09-07 19:58:54 +0100

    task

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,7 +10,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
     if (size == 0 || nmemb == 0)
         return (NULL);
     
-    mallocInt = malloc(size * nmemb);
+    mallocInt = calloc(nmemb, size);
     if (mallocInt == NULL)
         return (NULL);
     for (i = 0; i < nmemb; i++)
