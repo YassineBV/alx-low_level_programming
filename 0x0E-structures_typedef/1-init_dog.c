8b3e5f63c6c5e15039c11dd5628b7d95df3a35ef
YassineBV <businessarto@gmail.com>
2023-09-11 10:15:38 +0100

    task

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		exit(0);
+	}
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
