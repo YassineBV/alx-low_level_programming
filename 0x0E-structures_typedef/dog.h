71b9d60d5c244bfffa1379a13a766c007049b759
YassineBV <businessarto@gmail.com>
2023-09-11 10:37:41 +0100

    task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,10 +3,6 @@
 
 #include <stdio.h>
 
-struct dog {
-    char *name;
-    float age;
-    char *owner;
-};
+struct dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif /* DOG_H */
