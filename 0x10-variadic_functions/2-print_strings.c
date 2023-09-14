6c253eec0467a23db8cada33d5830408304248ed
YassineBV <businessarto@gmail.com>
2023-09-14 13:01:13 +0100

    task

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,8 +15,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(str_ptr, n);
 
-	if (str_ptr == "")
-			printf("nil");
+
+	    
 
 	for (i = 0; i < n; i++)
 	{
@@ -24,7 +24,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		char *str_char = va_arg(str_ptr, char *);
 
-		
+		if (!str_char)
+			printf("nil");
 
 		printf("%s", str_char);
 
