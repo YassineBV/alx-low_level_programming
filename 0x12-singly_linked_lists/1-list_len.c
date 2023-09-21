ddb4d8df9a30a2bf73f7ff45d60bf56327824583
YassineBV <businessarto@gmail.com>
2023-09-21 19:10:46 +0100

    task0

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * list_len - counts the number of nodes in a list_t
+ * @h: a pointer to the head node of the linked list
+ *
+ * Return: the number of nodes in the linked list
+ */
+size_t list_len(const list_t *h)
+{
+    size_t nd_count = 0;
+
+    
+    if (h != NULL)
+    {
+        nd_count++;
+        while (h->next != NULL)
+        {
+            nd_count++;
+            h = h->next;
+        }
+        
+        
+    }
+    return (nd_count);
+
+}
