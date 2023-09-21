d3576e1fa4da53d53e7350e9323f0a9f42f57052
YassineBV <businessarto@gmail.com>
2023-09-21 11:35:08 +0100

    task0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,23 +2,20 @@
 size_t print_list(const list_t *h)
 {
     int count_nods = 0;
-    list_t *ptr_nods = NULL;
 
-    if (h->str == NULL)
+    while (h != NULL)
     {
-        printf("[0] (nil)\n");
-    }
-    ptr_nods = h->next;
+        if (h->next == NULL)
+        {
+            printf("[0] (nil)\n");
+        }
+        else
+        {
+            printf("[%d] %s\n", h->len, h->str);
+            count_nods++;
+            h = h->next;
+        }
 
-    while (ptr_nods != NULL)
-    {
-        printf("[%d] %s\n", h->len, h->str);
-        count_nods++;
-        printf("[%d] %s\n", ptr_nods->len, ptr_nods->str);
-        count_nods++;
-        ptr_nods = ptr_nods->next;
-    }
-    
     return (count_nods);
 }
 
