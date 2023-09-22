c875d2f42499b0f9ddb3c383745fdc3dc8240c15
YassineBV <businessarto@gmail.com>
2023-09-22 12:04:28 +0100

    task0

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * print_me_first - function that prints a message.
+ *
+ * This function is executed before the main function when
+ * the program starts.
+ */
+void __attribute__ ((constructor)) print_me_first()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+
+}
