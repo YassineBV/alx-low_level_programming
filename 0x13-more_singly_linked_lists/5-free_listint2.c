0e0cf37e5c14e6f6782baf98b17c813a45aebd19
YassineBV <businessarto@gmail.com>
2023-09-25 19:37:49 +0100

    task0

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - Frees all listint_t list allocated memory.
+ * @head: Pointer to the head of the list.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *free_AllNds;
+
+
+	while (*head != NULL)
+	{
+		free_AllNds = *head;
+		*head = (*head)->next;
+		free(free_AllNds);
+	}
+
+}
