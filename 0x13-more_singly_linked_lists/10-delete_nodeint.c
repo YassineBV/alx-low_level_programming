962bda0e331a9171ca46e57251abf590d306d0cf
YassineBV <businessarto@gmail.com>
2023-09-26 18:03:53 +0100

    task0

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -23,6 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		*head = delet_ptr->next;
 		free(delet_ptr);
 		delet_ptr = NULL;
+		return (1);
 	}
 	else
 	{
@@ -39,8 +40,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			delet_ptr = delet_ptr->next;
 			inx++;
 		}
-
 	}
 	return (-1);
-
 }
