c96f2a9b3241ebd4a600f30189088e61c9246dab
YassineBV <businessarto@gmail.com>
2023-09-30 14:18:35 +0100

    task0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,18 @@
+#include "main.h"
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int x = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (*b != '\0')
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+		x = x * 2 + (*b - 48);
+		b++;
+
+	}
+	return (x);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,8 @@
+#ifndef BITW_M
+#define BITW_M
+
+#include <stdio.h>
+unsigned int binary_to_uint(const char *b);
+
+#endif
+
