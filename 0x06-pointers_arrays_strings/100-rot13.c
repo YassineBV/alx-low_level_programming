22a54fdcba6c5b28ae44435be036c67f58bea635
YassineBV <businessarto@gmail.com>
2023-10-18 17:50:40 +0100

    task

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,30 +9,17 @@
 char *rot13(char *ui)
 {
 	int i, j;
-	char toro[26][2] = {
-		{'a', 'A'}, {'b', 'B'}, {'c', 'C'}, {'d', 'D'}, {'e', 'E'},
-		{'f', 'F'}, {'g', 'G'}, {'h', 'H'}, {'i', 'I'}, {'j', 'J'},
-		{'k', 'K'}, {'l', 'L'}, {'m', 'M'}, {'n', 'N'}, {'o', 'O'},
-		{'p', 'P'}, {'q', 'Q'}, {'r', 'R'}, {'s', 'S'}, {'t', 'T'},
-		{'u', 'U'}, {'v', 'V'}, {'w', 'W'}, {'x', 'X'}, {'y', 'Y'},
-		{'z', 'Z'}
-	};
-	char ro13[26][2] = {
-		{'N', 'n'}, {'O', 'o'}, {'P', 'p'}, {'Q', 'q'}, {'R', 'r'},
-		{'S', 's'}, {'T', 't'}, {'U', 'u'}, {'V', 'v'}, {'W', 'w'},
-		{'X', 'x'}, {'Y', 'y'}, {'Z', 'z'}, {'A', 'a'}, {'B', 'b'},
-		{'C', 'c'}, {'D', 'd'}, {'E', 'e'}, {'F', 'f'}, {'G', 'g'},
-		{'H', 'h'}, {'I', 'i'}, {'J', 'j'}, {'K', 'k'}, {'L', 'l'},
-		{'M', 'm'}
-	};
+	char toro[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ro13[52] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
 
 	for (i = 0; ui[i] != '\0'; i++)
 	{
-		for (j = 0; j < 26; j++)
+		for (j = 0; j < 52; j++)
 		{
-			if (ui[i] == toro[j][0] || ui[i] == toro[j][1])
+			if (ui[i] == toro[j])
 			{
-				ui[i] = (ui[i] ==  toro[j][0]) ? ro13[j][1] : ro13[j][0];
+				ui[i] = ro13[j];
 				break;
 			}
 		}
