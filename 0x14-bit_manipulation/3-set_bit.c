27f79c00458f501d30c61b1d126a662a013fe8ea
YassineBV <businessarto@gmail.com>
2023-11-24 12:56:27 +0100

    task

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,7 +4,7 @@ int set_bit(unsigned long int *n, unsigned int index)
     
 
     *n |=  (1UL << index);
-    if (*n >= ULONG_MAX || index >= UINT_MAX)
+    if (*n >= ULONG_MAX || index >= 64)
         return (-1);
     return (1);
 }
