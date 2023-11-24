e563f72978144e58bc914be5686231a8783048c9
YassineBV <businessarto@gmail.com>
2023-11-24 08:40:30 +0100

    task

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,12 +12,14 @@ void print_binary(unsigned long int n)
 	if (n == 1)
 		_putchar('1');
 	if (n > 1 && n < 128)
-		for (i = 6; i >= 0; i-- )
 	{
-		x = n & (1 << i);
-		if (x != 0)
-		    x = 1;
-		_putchar(x + 48);
+		for (i = 6; i >= 0; i-- )
+	    {
+		    x = n & (1 << i);
+		    if (x != 0)
+		        x = 1;
+		    _putchar(x + 48);
+	    }
 	}
 	if (n > 128 && n < 2048)
 	{
@@ -30,6 +32,4 @@ void print_binary(unsigned long int n)
 
 	    }
 	}
-
-
 }
