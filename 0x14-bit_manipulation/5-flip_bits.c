48e85b9db3fbecf144982301fc7c42d352901798
YassineBV <businessarto@gmail.com>
2023-11-24 15:53:06 +0100

    task

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * flip_bits - Counts the number of differing bits between two integers.
+ * @n: The first unsigned long integer.
+ * @m: The second unsigned long integer.
+ *
+ * Return: The number of bits that differ between the two integers.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+    unsigned long int x, i;
+    int count = 0;
+
+    x = sizeof(unsigned long int) * 8;
+
+    for (i = 0; i < x; i++)
+    {
+        bool x = false;
+        bool z = false;
+        if (n & (1Ul << i))
+            x = true;
+        if (m & (1UL << i))
+            z = true;
+        if (z == false)
+        {
+            if (x == true)
+                count++;
+        }
+        if (z == true)
+        {
+            if (x == false)
+               count++;
+        }
+
+    }
+    return (count);
+}
