3a2da2dc06744ef1451624fd577cf1539841787e
YassineBV <businessarto@gmail.com>
2023-11-24 12:33:48 +0100

    task

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,11 +1,14 @@
 #include"main.h"
+#include <limits.h>
 int get_bit(unsigned long int n, unsigned int index)
 {
     int bitstat;
 
-    bitstat = n & (1 << index);
+    bitstat = n & (1UL << index);
     if (bitstat != 0)
         return (1);
+    if (n > ULONG_MAX)
+        return (-1);
     return (0);
 
 }
