0720b2be97901756bb56caaf8ba10ce54555d647
YassineBV <businessarto@gmail.com>
2023-11-27 16:06:11 +0100

    task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,18 +12,19 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+
 	if (text_content == NULL)
-	    text_content = "";
+		text_content = "";
 
 	fds = open(filename, O_RDONLY | O_WRONLY | O_CREAT, 0600);
 	if (fds < 0)
-	    return (-1);
+		return (-1);
 	txtLent = strlen(text_content);
 
 	wrchk = write(fds, text_content, txtLent);
 
 	if (wrchk != txtLent)
-	    return (-1);
+		return (-1);
 
 	return (1);
 
