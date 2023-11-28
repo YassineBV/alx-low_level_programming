b7d1a8527e2ab1305057e746122011da04de2aa9
YassineBV <businessarto@gmail.com>
2023-11-28 16:38:31 +0100

    task

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,22 +4,34 @@
 #include <fcntl.h>
 #include <string.h>
 void closeFiles(int fd1, int fd2);
+/**
+ * closeFiles - Closes two file descriptors and handles errors.
+ * @fd1: First file descriptor.
+ * @fd2: Second file descriptor.
+ */
 void closeFiles(int fd1, int fd2)
 {
-    int clos1 = close(fd1);
-    int clos2 = close(fd2);
+	int clos1 = close(fd1);
+	int clos2 = close(fd2);
 
-    if (clos1 == -1)
-    {
-        dprintf(2, "Error: Can't close fd %d\n", fd1);
-        exit(100);
-    }
-    if (clos2 == -1)
-    {
-        dprintf(2, "Error: Can't close fd %d\n", fd2);
-        exit(100);
-    }
+	if (clos1 == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	if (clos2 == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
 }
+/**
+ * main - Copies content from one file to another.
+ * @ac: Number of arguments.
+ * @av: Array of arguments.
+ *
+ * Return: 0 on success, exits with error codes on failure.
+ */
 int main(int ac, char **av)
 {
 	int fd1, fd2, toread, toWrit;
@@ -27,7 +39,7 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 	{
-		dprintf(2,"Usage: cp file_from file_to\n");
+		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	file_from = av[1];
@@ -45,19 +57,19 @@ int main(int ac, char **av)
 		exit(99);
 	}
 	while ((toread = read(fd1, reaBuf, 1024)) > 0)
-    {
-        if (toread == -1)
-    {
-        dprintf(2, "Error: Can't read from file %s\n", av[1]);
-        exit(98);
-    }
-        toWrit = write(fd2, reaBuf, toread);
-        if (toWrit < 0)
-        {
-            dprintf(2, "Error: Can't write to %s\n", av[2]);
-            exit(99);
-        }
-    }
+	{
+		if (toread == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+		toWrit = write(fd2, reaBuf, toread);
+		if (toWrit < 0)
+		{
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	}
 	closeFiles(fd1, fd2);
 	return (0);
 }
