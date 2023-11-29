02583d818b6f9fb60b11bddd660929db2a732aaa
YassineBV <businessarto@gmail.com>
2023-11-29 13:59:55 +0100

    task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -32,7 +32,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	bufread[toread] = '\0';
-	toWrite = write(1, bufread, toread);
+	toWrite = write(STDOUT_FILENO, bufread, toread);
 	if (toWrite == -1)
 		return (0);
 	free(bufread);
