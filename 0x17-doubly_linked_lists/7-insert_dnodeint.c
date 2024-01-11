31279631a0d0d19177fa6027c6b51f9f9e019f1c
YassineBV <bussinessarto@gmail.com>
2024-01-11 18:55:17 +0100

    task

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,36 +14,37 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (*h == NULL)
 		return (NULL);
+
 	my_node = malloc(sizeof(dlistint_t));
 	if (my_node == NULL)
 		return (NULL);
-	temp = *h;
+
 	my_node->prev = NULL;
 	my_node->n = n;
 	my_node->next = NULL;
+	temp = *h;
 	if (idx == 0)
 	{
 		my_node->next = temp;
-		my_node->prev = NULL;
+		if (temp != NULL)
+			temp->prev = my_node;
 		*h = my_node;
 		return (my_node);
 	}
-	else
-		while (temp != NULL)
-		{
-			if (i == idx - 1)
-			{
-				if (temp->next == NULL)
-					my_node->next = NULL;
-				else
-					my_node->next = temp->next;
-				temp->next = my_node;
-				my_node->prev = temp;
-				return (my_node);
-			}
-			temp = temp->next;
-			i++;
-		}
-	return (NULL);
 
+	while (temp != NULL)
+	{
+		if (i == idx - 1)
+		{
+			my_node->next = temp->next;
+			if (temp->next != NULL)
+				temp->next->prev = my_node;
+			temp->next = my_node;
+			my_node->prev = temp;
+			return (my_node);
+		}
+		temp = temp->next;
+		i++;
+	}
+	return (NULL);
 }
