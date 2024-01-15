365655dfa812608bfe6b08a6d1202dbd9feed804
YassineBV <bussinessarto@gmail.com>
2024-01-15 16:07:05 +0100

    task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/YassineBV/alx-low_level_programming/0x18-dynamic_libraries/mygiga_lib.so
-export LD_PRELOAD=/tmp/mygiga_lib.so
+wget -P /tmp https://https://github.com/YassineBV/alx-low_level_programming/0x18-dynamic_libraries/mygigalib.so
+export LD_PRELOAD=/tmp/mygigalib.so
