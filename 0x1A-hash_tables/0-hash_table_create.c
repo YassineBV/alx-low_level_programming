ffd7daf0c62f6a835d98bd2c55b35933c4b2983c
YassineBV <bussinessarto@gmail.com>
2024-02-03 16:24:59 +0100

    task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,10 +7,21 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *create = malloc(sizeof(hash_table_t));
+	hash_table_t *create_table = NULL;
 
-	create->size = size;
-	create->array = malloc(size * sizeof(hash_node_t));
+	create_table = malloc(sizeof(hash_table_t));
 
-	return (create);
+	if (create_table == NULL)
+		return (NULL);
+
+	create_table->size = size;
+	create_table->array = malloc(size * sizeof(hash_node_t *));
+
+	if (create_table->array == NULL)
+	{
+		free(create_table);
+		return (NULL);
+	}
+
+	return (create_table);
 }
